dbcf8cd2ca8b5a9769eba2558e1caa7333058acc
DamianK77 <damik422@gmail.com>
2025-03-14 15:12:02 +0100

    include libraries

--- main/main.c
+++ main/main.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+#include <math.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "driver/i2c_master.h"
+#include "esp_log.h"
+#include "esp_timer.h"
+#include "esp_mpu6050.h"
 
 void app_main(void)
 {
